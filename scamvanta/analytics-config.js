1170eb4ef10f6151fe5d3763476a6bb510e8e216
scamvanta <archithgaddam2@gmail.com>
2026-09-25 10:40:23 -0400

    Add files via upload

---
+++ scamvanta/analytics-config.js
@@ -0,0 +1,7 @@
+/* Optional GA4 analytics. Disabled until the owner completes README setup.
+   Paste your real web-stream Measurement ID in measurementId, then set enabled
+   to true. Visitors must also opt in before any Google script is requested. */
+window.SCAMVANTA_ANALYTICS_CONFIG = Object.freeze({
+  enabled: false,
+  measurementId: '' // Paste your G-... Measurement ID between these quotes.
+});
