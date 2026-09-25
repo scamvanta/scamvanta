dd9965335dbe4ddbf8fe0568fa50581cb918a4a5
scamvanta <archithgaddam2@gmail.com>
2026-09-25 17:23:25 -0400

    Connect opt-in analytics to ScamVanta GA4 stream

--- analytics-config.js
+++ analytics-config.js
@@ -2,6 +2,6 @@
    Paste your real web-stream Measurement ID in measurementId, then set enabled
    to true. Visitors must also opt in before any Google script is requested. */
 window.SCAMVANTA_ANALYTICS_CONFIG = Object.freeze({
-  enabled: false,
-  measurementId: '' // Paste your G-... Measurement ID between these quotes.
+  enabled: true,
+  measurementId: 'G-VWTVS56WF2' // ScamVanta web stream; visitors must opt in.
 });
